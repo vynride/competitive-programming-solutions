1daa6cb5915e8b7667afc7f825b36161618681ff
Vivian <vynride@gmail.com>
2026-03-09 00:42:26 +0530

    chore: 1980. Find Unique Binary String

---
+++ leetcode/1980_Find_Unique_Binary_String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+
+        string ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i][i] == '0') ans.push_back('1');
+            else ans.push_back('0');
+        }
+
+        return ans;
+    }
+};
